eab86ab7ca639e485e22c79ce0b9d6e97120cc74
JullyLima <160797396+JullyLima@users.noreply.github.com>
2024-03-26 13:59:06 -0300

    semana5

---
+++ semana5.c/atv10.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include <stdlib.h>
+void iguais(int lenA, int lenB, int a[lenA], int b[lenB]){
+    int lenf= lenA+lenB;
+    int count=0;
+    int uniao[lenf];
+
+    for (int i=0;i<lenA;i++){
+        uniao[count++] = a[i];
+    }
+    for(int j=0;j<lenB;j++){
+            uniao[count++] = b[j];
+    }
+
+
+   printf("\nA união dos dois conjuntos são: ");
+    for (int i = 0; i < count; i++) {
+        printf("%d ", uniao[i]);
+    }
+}
+
+
+
+int main (){
+
+    int lenx,leny;
+
+    printf("dígite o tamanho da lista 1: ");
+    scanf("%d",&lenx);
+    int x[lenx];
+
+   for (int i =0; i <lenx; i++) {
+       printf("digite o valor para a posição %d: ",i);
+       scanf("%d",&x[i]);
+   }
+      printf("\nLista1: ");
+    for (int i = 0; i < lenx; i++) {
+        printf("%d ", x[i]);
+    }
+
+    printf("\ndígite o tamanho da lista 2: ");
+    scanf("%d",&leny);
+    int y[leny];
+
+   for (int i =0; i <leny; i++) {
+       printf("digite o valor para a posição %d: ",i);
+       scanf("%d",&y[i]);
+   }
+    printf("\nLista2: ");
+    for (int i = 0; i < leny; i++) {
+        printf("%d ", y[i]);
+    }
+
+    iguais(lenx, leny,x,y);
+
+  return 0;  
+}
